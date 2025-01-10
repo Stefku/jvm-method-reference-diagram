97ac43e3b6f0d8b95a2c5f6d5438f0792b10b4e9
GitHub Action <action@github.com>
2025-01-10 07:45:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/stefku/jvmmethodreferencediagram/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stefku/jvmmethodreferencediagram/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.stefku.jvmmethodreferencediagram.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stefku.jvmmethodreferencediagram.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
